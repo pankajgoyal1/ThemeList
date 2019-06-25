02bb3685478eb75ae125748aac5a3b76f33dddda
Pankaj Goyal <pkjg1807@gmail.com>
2019-06-25 13:27:58 +0530

    addded files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Components/Header/Header';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import ThemeList from './Components/ThemeList/ThemeList';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      data:[]
+    }
+  }
+  render(){
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={ThemeList} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
+  }
 }
 
 export default App;
